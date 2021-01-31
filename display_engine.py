2ddea12fb2215ff1fe5e05eff914cefe253c990a
Piotr Skrobski <piotrskrobski2001@protonmail.com>
2021-01-31 14:52:31 +0100

    added clock feature

--- display_engine.py
+++ display_engine.py
@@ -14,20 +14,34 @@ class DisplayWindow():
         self.window.geometry(str(self.dim_x) + "x" + str(self.dim_y))
 
 
-        btn_choosefile = tk.Button(
+        self.btn_choosefile = tk.Button(
             master = self.window,
             text = "Open the file",
             command = self.choose_file
         )
 
-        btn_playmusic = tk.Button(
+        self.btn_pausemusic = tk.Button(
+            master = self.window, 
+            text = "Pause music!",
+            command = lambda : MusicEngine.PauseMusic(MusicEngine)
+        )
+
+        self.btn_playmusic = tk.Button(
             master = self.window, 
             text = "Play music!",
             command = lambda : MusicEngine.LoadMusic(self, self.filepath) #I have no clue why making this lambda works, but it works.
             )
+
+        self.lbl_time = tk.Label(
+            master = self.window,
+            text = '0:00'
+        )
         
-        btn_choosefile.grid(row = 0, column = 0, sticky="nw")
-        btn_playmusic.grid(row = 1, column = 0)
+        self.clock()
+        self.btn_choosefile.grid(row = 0, column = 0, sticky = "nw")
+        self.btn_playmusic.grid(row = 1, column = 0, sticky = "nw")
+        self.btn_pausemusic.grid(row = 1, column = 1)
+        self.lbl_time.grid(row = 2, column = 0, sticky="ns")
 
     def choose_file(self):
         self.filepath = askopenfilename(
@@ -35,6 +49,25 @@ class DisplayWindow():
         )
         if not self.filepath:
             return
+    
+    def get_time_of_song(self):
+        return MusicEngine.GetTimeOfSong(MusicEngine)
+
+    def clock(self):
+        if MusicEngine.is_music_playing == True:
+            seconds = self.get_time_of_song() / 1000
+            minutes = 0
+            if seconds > 59:
+                while seconds > 59:
+                    seconds -= 60
+                    minutes += 1
+
+            seconds = round(seconds)
+            if seconds < 10:
+                seconds = "0" + str(seconds)
+            self.lbl_time["text"] = str(minutes) + ":" + str(seconds)
+        self.window.after(1000, self.clock)
+
         
 
 
--- sound_engine.py
+++ sound_engine.py
@@ -2,16 +2,21 @@ import pygame
 import os.path
 
 
+
 class MusicEngine():
+
+    is_music_playing = False
+    is_music_paused = False
 
     def __init__(self):
         #Initalise music engine
         pygame.mixer.init()
-        self.is_music_playing = False
+        
     
     def LoadMusic(self, filename, loops = None):
         if os.path.exists(filename):
             pygame.mixer.music.load(filename)
+            MusicEngine.is_music_playing = True
             if loops != None:
                 pygame.mixer.music.play(loops)
             else:
@@ -20,10 +25,21 @@ class MusicEngine():
         else:
             return -1 #File does not exist, error
 
+    def PauseMusic(self):
+            if MusicEngine.is_music_paused == False:
+                pygame.mixer.music.pause()
+                MusicEngine.is_music_paused = True
+            else:
+                pygame.mixer.music.unpause()
+
+
     def PlayMusic(self, loops = None):
         if loops != None:
             pygame.mixer.music.play(loops)
         else:
             pygame.mixer.music.play()
+
+    def GetTimeOfSong(self):
+        return pygame.mixer.music.get_pos()
 
 
